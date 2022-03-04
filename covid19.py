98b212d986ebd1ce64eb98b6dc2f4e42ebb15335
Fernando <fernando22future@gmail.com>
2022-03-04 20:51:36 -0300

    Update

--- covid19.py
+++ covid19.py
@@ -8,8 +8,11 @@ import pandas as pd
 covid = pd.read_csv('us.csv', parse_dates=['date'])
 covid.set_index('date', inplace=True)
 
+plt.rcParams['font.family'] = 'sans-serif'
+plt.style.use('ggplot')
+
 # Creates the graphics that shows the data
-fig = plt.figure()
+fig = plt.figure(figsize=(7, 7), constrained_layout=True)
 ax = fig.add_subplot(1,1,1)
 
 ax.plot(covid['cases'], color='tab:green', label='Cases')
@@ -17,20 +20,22 @@ ax.plot(covid['deaths'], color='tab:red', label='Deaths')
 
 #creates the legend
 leg = ax.legend(loc='upper left', fancybox=True, shadow=True)
-leg.get_frame().set_alpha(0.4)
+leg.get_frame().set_alpha(0.2)
 
 #set ticks every week
-ax.xaxis.set_major_locator(mdates.DayLocator(interval=15))
+ax.xaxis.set_major_locator(mdates.DayLocator(interval=20))
 #set major ticks format
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %B'))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %B %Y'))
 
 ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
 
 #Sets x, y and title labels
 ax.set_xlabel('Date', fontsize = 10)
-ax.set_ylabel('Number of occurency', fontsize=10)
+ax.set_ylabel('Number of occurency', fontsize = 10)
 ax.set_title('Number of cases and deaths in US')
 
-plt.xticks(rotation=45)
+ax.grid(True)
+
+plt.xticks(rotation=50)
 
 plt.show()
