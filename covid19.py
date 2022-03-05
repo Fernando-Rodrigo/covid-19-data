4eb3a60654a8f9268ef8e497d9948dd8f3a5cd00
Fernando <fernando22future@gmail.com>
2022-03-05 15:32:37 -0300

    Update

--- covid19.py
+++ covid19.py
@@ -10,35 +10,52 @@ import pandas as pd
 covid = pd.read_csv('us.csv', parse_dates=['date'])
 covid.set_index('date', inplace=True)
 
+# Data Screening
+covid.shape
+covid.columns
+
+# Data Cleaning
+sum(covid.groupby("date").cases.sum().diff() < 0)
+
 plt.rcParams['font.family'] = 'sans'
 plt.rcParams['font.weight'] = 'semibold'
 plt.style.use('ggplot')
 
 # Creates the graphics that shows the data
-fig = plt.figure(figsize=(7, 7))
-ax = fig.add_subplot(1,1,1)
+fig = plt.figure(figsize=(5, 5))
+ax = fig.add_subplot(2,2,1)
+ax1 = fig.add_subplot(2,2,2)
 
 ax.plot(covid['cases'], color='tab:green', label='Cases')
-ax.plot(covid['deaths'], color='tab:red', label='Deaths')
-
-#creates the legend
-leg = ax.legend(loc='upper left', fancybox=True, shadow=True)
-leg.get_frame().set_alpha(0.2)
+ax1.plot(covid['deaths'], color='tab:red', label='Deaths')
 
 #set ticks every week
-ax.xaxis.set_major_locator(mdates.DayLocator(interval=20))
+ax.xaxis.set_major_locator(mdates.DayLocator(interval=50))
 #set major ticks format
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %B %Y'))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %b %y'))
 
 ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
 
 #Sets x, y and title labels
 ax.set_xlabel('Date', fontsize = 10, family='sans', weight='semibold')
 ax.set_ylabel('Number of occurency', fontsize = 10, family='sans', weight='semibold')
-ax.set_title('Number of cases and death in US')
+ax.set_title('Number of cases in US')
+
+# Rotates and right-aligns the x labels so they don't crowd each other.
+for label in ax.get_xticklabels(which='major'):
+    label.set(rotation=45, horizontalalignment='right')
+
+ax1.set_title('Number of death in US')
+ax1.xaxis.set_major_locator(mdates.DayLocator(interval=50))
+ax1.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
+ax1.xaxis.set_major_formatter(mdates.DateFormatter('%d %b %y'))
+ax1.set_xlabel('Date', fontsize = 10, family='sans', weight='semibold')
+for label in ax1.get_xticklabels(which='major'):
+    label.set(rotation=45, horizontalalignment='right')
 
 ax.grid(True)
+ax1.grid(True)
 
-plt.xticks(rotation=50)
+#plt.xticks(rotation=50)
 
 plt.show()
