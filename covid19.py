448695537db27cdfbbb862a27722cff9cb073cb7
Fernando Rodrigo Bairros de Pilger <49997831+Fernando-Rodrigo@users.noreply.github.com>
2023-04-07 17:46:43 -0300

    covid19.py update
    I only updated the covid19.py for now.

--- covid19.py
+++ covid19.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-import matplotlib as mpl
+import matplotlib.ticker as tk
 import pandas as pd
 
 # Reads the csv file, where the data is saved
@@ -19,20 +19,20 @@ plt.rcParams['font.weight'] = 'semibold'
 plt.style.use('ggplot')
 
 # Creates the graphics that shows the data
-fig = plt.figure(figsize=(5, 5))
-ax = fig.add_subplot(2,2,1)
-ax1 = fig.add_subplot(2,2,2)
+plt.figure(1)
+ax = plt.subplot(211)
+ax1 = plt.subplot(212)
 
 ax.plot(covid['cases'], color='tab:green', label='Cases')
 ax1.plot(covid['deaths'], color='tab:red', label='Deaths')
 
 #Number of cases analysis
 #set ticks every week
-ax.xaxis.set_major_locator(mdates.DayLocator(interval=50))
+ax.xaxis.set_major_locator(mdates.DayLocator(interval=85))
 
 #set major ticks format
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %b %y'))
-ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
+ax.yaxis.set_major_formatter(tk.StrMethodFormatter('{x:,.0f}'))
 
 #Sets x, y and title labels
 ax.set_xlabel('Date', fontsize = 10, family='sans', weight='semibold')
@@ -40,17 +40,17 @@ ax.set_ylabel('Number of occurency', fontsize = 10, family='sans', weight='semib
 ax.set_title('Cases in US')
 
 # Rotates and right-aligns the x labels so they don't crowd each other.
-for label in ax.get_xticklabels(which='major'):
-    label.set(rotation=45, horizontalalignment='right')
+for label in ax.get_xticklabels(which='major'):    label.set(rotation=0, horizontalalignment='center')
 
 #Number of death analysis
 ax1.set_title('Death in US')
-ax1.xaxis.set_major_locator(mdates.DayLocator(interval=50))
-ax1.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
+ax1.xaxis.set_major_locator(mdates.DayLocator(interval=85))
+ax1.yaxis.set_major_formatter(tk.StrMethodFormatter('{x:,.0f}'))
+ax1.set_ylabel('Number of occurency', fontsize = 10, family='sans', weight='semibold')
 ax1.xaxis.set_major_formatter(mdates.DateFormatter('%d %b %y'))
 ax1.set_xlabel('Date', fontsize = 10, family='sans', weight='semibold')
 for label in ax1.get_xticklabels(which='major'):
-    label.set(rotation=45, horizontalalignment='right')
+    label.set(rotation=0, horizontalalignment='center')
 
 ax.grid(True)
 ax1.grid(True)
