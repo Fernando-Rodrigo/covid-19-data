99e554acbd5b85155db421434c3aa720fafe1ec6
Fernando Rodrigo Bairros de Pilger <49997831+Fernando-Rodrigo@users.noreply.github.com>
2026-07-12 21:09:15 -0300

    Update

--- covid19.py
+++ covid19.py
@@ -26,29 +26,30 @@ ax1 = plt.subplot(212)
 ax.plot(covid['cases'], color='tab:green', label='Cases')
 ax1.plot(covid['deaths'], color='tab:red', label='Deaths')
 
-#Number of cases analysis
-#set ticks every week
+# Number of cases analysis
+# set ticks every week
 ax.xaxis.set_major_locator(mdates.DayLocator(interval=85))
 
-#set major ticks format
+# set major ticks format
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %b %y'))
 ax.yaxis.set_major_formatter(tk.StrMethodFormatter('{x:,.0f}'))
 
-#Sets x, y and title labels
-ax.set_xlabel('Date', fontsize = 10, family='sans', weight='semibold')
-ax.set_ylabel('Number of occurency', fontsize = 10, family='sans', weight='semibold')
+# Sets x, y and title labels
+ax.set_xlabel('Date', fontsize=10, family='sans', weight='semibold')
+ax.set_ylabel('Number of occurency', family='sans', weight='semibold')
 ax.set_title('Cases in US')
 
 # Rotates and right-aligns the x labels so they don't crowd each other.
-for label in ax.get_xticklabels(which='major'):    label.set(rotation=0, horizontalalignment='center')
+for label in ax.get_xticklabels(which='major'):
+    label.set(rotation=0, horizontalalignment='center')
 
-#Number of death analysis
+# Number of death analysis
 ax1.set_title('Death in US')
 ax1.xaxis.set_major_locator(mdates.DayLocator(interval=85))
 ax1.yaxis.set_major_formatter(tk.StrMethodFormatter('{x:,.0f}'))
-ax1.set_ylabel('Number of occurency', fontsize = 10, family='sans', weight='semibold')
+ax1.set_ylabel('Number of occurency', family='sans', weight='semibold')
 ax1.xaxis.set_major_formatter(mdates.DateFormatter('%d %b %y'))
-ax1.set_xlabel('Date', fontsize = 10, family='sans', weight='semibold')
+ax1.set_xlabel('Date', fontsize=10, family='sans', weight='semibold')
 for label in ax1.get_xticklabels(which='major'):
     label.set(rotation=0, horizontalalignment='center')
 
